bb27711d9201b6de266bddc9e98cd7079e7c9ea6
IgorGarciaRez <igorgarciarezende05@gmail.com>
2025-05-09 15:21:57 -0300

    Requisicoes pokemons

--- src/components/FlipCard.tsx
+++ src/components/FlipCard.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 import './FlipCard.css';
 
-const FlipCardBootstrap = () => {
+interface FlipCardProps {
+  name: string;
+  img: string;
+}
+
+export default function FlipCardBootstrap({ name, img }: FlipCardProps) {
   const [flipped, setFlipped] = useState(false);
 
   return (
-    <div
-      className={`flip-card ${flipped ? 'flipped' : ''}`}
-      onClick={() => setFlipped(!flipped)}
-    >
-      <div className="flip-card-inner">
-        <div className="flip-card-front d-flex align-items-center justify-content-center bg-primary text-white">
-          Frente
+    <div className={`flip-card m-2 ${flipped ? 'flipped' : ''}`} onClick={() => setFlipped(!flipped)}>
+      <div className={`flip-card-inner`}>
+        <div className="flip-card-front">
+          <img src={img} alt={name} />
+          <div className="card-body">
+            <h6 className="card-title mb-0 text-capitalize">{name}</h6>
+          </div>
         </div>
-        <div className="flip-card-back d-flex align-items-center justify-content-center bg-secondary text-white">
-          Verso
+
+        <div className="flip-card-back">
+          <p className="m-0">Verso da carta</p>
         </div>
       </div>
     </div>
   );
-};
-
-export default FlipCardBootstrap;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,38 @@
+import React, { useState } from "react"
+import { useEffect } from "react"
 import FlipCardBootstrap from "../components/FlipCard"
 import NavbarBootstrap from "../components/Navbar"
+import axios from "axios"
 
 export const Home = ()=> {
+
+    const [pokemons, setPokemons] = useState([]);    
+    useEffect(() => {
+        getPokemons();
+    }, []);
+
+    const getPokemons = () => {
+        var endpoints = [];
+        for (var i = 1; i < 50; i++){
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        }
+        var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+            .then((res) => setPokemons(res))
+            .catch((err) => console.log(err));
+
+
+        /*axios.get("https://pokeapi.co/api/v2/pokemon?limit=50")
+        .then((res) => setPokemons(res.data.results))
+        .catch((err) => console.log(err))*/
+    };
+    
     return (
         <div>
             <NavbarBootstrap/>
-            <div className="d-flex justify-content-center mt-5">
-                <FlipCardBootstrap/>
-                <FlipCardBootstrap/>
-                <FlipCardBootstrap/>
-                <FlipCardBootstrap/>
-                <FlipCardBootstrap/>
+            <div className="d-flex flex-wrap justify-content-center mt-5">
+                {pokemons.map((pokemon) => (
+                    <FlipCardBootstrap key={pokemon.data.name} name={pokemon.data.name} img={pokemon.data.sprites.front_default}/>
+                ))}
             </div>
             
         </div>
